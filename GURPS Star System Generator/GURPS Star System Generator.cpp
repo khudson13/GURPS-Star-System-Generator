cb7e9ad39642e5e8108f896e0ea32a016948b835
khudson13 <kendallhudson13@gmail.com>
2022-08-30 16:34:02 -0700

    test

--- GURPS Star System Generator/GURPS Star System Generator.cpp
+++ GURPS Star System Generator/GURPS Star System Generator.cpp
@@ -1,6 +1,11 @@
 // GURPS Star System Generator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+/*
+Plan to handle each selestial object as its own class, including a class to handle the general characteristics of the system as
+a whole.
+*/
+
 #include <iostream>
 
 int main()
