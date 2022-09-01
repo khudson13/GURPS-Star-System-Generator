88ef70ec09a03ce8c5895a2d12c50c428a8b7764
khudson13 <kendallhudson13@gmail.com>
2022-09-01 16:54:47 -0700

    set number of stars

--- GURPS Star System Generator/System_Stats.cpp
+++ GURPS Star System Generator/System_Stats.cpp
@@ -4,16 +4,39 @@
 // METHOD DEFINITIONS *
 //*********************
 
+// get number of stars
 int System_Stats::num_Stars()
 {
 	return num_of_stars;
 }
 
+// generate number of stars
 void System_Stats::gen_Stars()
 {
 	int roll_3D6{ 0 }; // result of roll
 
+	// roll 3D6
 	std::mt19937 gen(rando()); // seed the generator
 	std::uniform_int_distribution<> distr(3, 18); // define the range
 	roll_3D6 = distr(gen);
+
+	// interpret roll
+	if (roll_3D6 >= 3 && roll_3D6 <= 10)
+	{
+		set_Stars(1);
+	}
+	else if (roll_3D6 >= 11 && roll_3D6 <= 15)
+	{
+		set_Stars(2);
+	}
+	else if (roll_3D6 >= 16)
+	{
+		set_Stars(3);
+	}
+}
+
+// set number of stars
+void System_Stats::set_Stars(int x)
+{
+	num_of_stars = x;
 }
--- GURPS Star System Generator/System_Stats.h
+++ GURPS Star System Generator/System_Stats.h
@@ -8,9 +8,11 @@ private:
 	int num_of_stars;
 	std::random_device rando;  // obtain random number from hardware for rng seed
 	
+	// set number of stars in system
+	void set_Stars(int);
 
 public:
-
+	
 	// return number of stars in system
 	int num_Stars();
 
