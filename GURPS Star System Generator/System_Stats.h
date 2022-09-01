f630c44d02416ca36e6f4728c56e4a8efe57309f
khudson13 <kendallhudson13@gmail.com>
2022-08-31 21:17:16 -0700

    thinking about random number gen

--- GURPS Star System Generator/System_Stats.h
+++ GURPS Star System Generator/System_Stats.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <random>
+
 class System_Stats
 {
 private:
@@ -23,4 +26,11 @@ public:
 int System_Stats::num_Stars()
 {
 	return num_of_stars;
+}
+
+void System_Stats::gen_Stars()
+{
+	std::random_device rando;  // obtain random number from hardware
+	std::mt19937 gen(rando()); // seed the generator
+	std::uniform_int_distribution<> distr(25, 63); // define the range
 }
